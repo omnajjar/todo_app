a766868d137dd6cec826eb5cfee1e5b8fa4786ee
Osama Najjar <osama.najjar.m@gmail.com>
2020-05-12 11:21:12 +0200

    Finish tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Startup Name Generator",
-        home: RandomWords()
+      title: "Startup Name Generator",
+      home: RandomWords(),
+      theme: ThemeData(primaryColor: Colors.greenAccent),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _savedWords = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions() => ListView.builder(
+
+  Widget _buildSuggestions() =>
+      ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return Divider();
@@ -30,15 +34,55 @@ class RandomWordsState extends State<RandomWords> {
         },
       );
 
-  Widget _buildRow(WordPair pair) => ListTile(
-          title: Text(
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _savedWords.contains(pair);
+    return ListTile(
+      title: Text(
         pair.asPascalCase,
         style: _biggerFont,
-      ));
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() =>
+            alreadySaved ? _savedWords.remove(pair) : _savedWords.add(pair));
+      },
+    );
+  }
+  
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles =
+          _savedWords.map((WordPair pair) => ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+            onTap: () {
+
+            },
+              ));
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Saved Suggestion"),
+          ),
+          body: ListView(children: divided));
+    }));
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text("Startup Name Generator")),
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
       body: _buildSuggestions());
 }
 
